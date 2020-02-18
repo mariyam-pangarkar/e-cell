ee277d7af12a3c86f04fb1ea28b34e9c02c69f08
Gazala <31682334+gazalaQuresh@users.noreply.github.com>
2020-02-18 16:16:48 +0530

    Add files via upload

---
+++ index.blade.php
@@ -0,0 +1,29 @@
+@include('/partials/header')
+@include('/partials/nav')
+@include('/partials/sidebar')
+        <!-- Breadcrumbs-->
+        <ol class="breadcrumb">
+          <li class="breadcrumb-item">
+            <a href="#">Dashboard</a>
+          </li>
+          <li class="breadcrumb-item active">Overview</li>
+        </ol>
+
+        <!-- Icon Cards-->
+        <div class="row">
+          <div class="col-xl-3 col-sm-6 mb-3">
+            <div class="card text-white bg-primary o-hidden h-100">
+              <div class="card-body">
+                <div class="fa fa-cog">
+                
+                </div>
+                <div class="mr-5">EXAM-CELL</div>
+              </div>
+            
+        
+       @include('/partials/footer')  
+
+
+
+
+
